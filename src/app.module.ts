429e61d13cd4bd3ddd33f77bc116763457e9ddda
mrxx1234m <uzbsamuraypubgm@example.com>
2025-07-10 18:46:12 +0500

    user update

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { CoreModule } from './core/core.module';
 import { AuthorizationModule } from './authorization/authorization.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [CoreModule, AuthorizationModule, AuthModule,ConfigModule.forRoot({
     isGlobal: true, 
     envFilePath: '.env',
-  }),],
+  }),UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
