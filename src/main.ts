78a1757463fd5ef878032908dba726b7ff77f855
mrxx1234m <uzbsamuraypubgm@example.com>
2025-07-13 22:11:02 +0500

    update

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // 🔁 CORS SOZLAMA BIRINCHI TURISHI KERAK
-  // app.enableCors({
-  //   origin: 'http://localhost:5173',
-  //   credentials: true,
-  //   methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  //   allowedHeaders: 'Origin, Content-Type, Accept, Authorization',
-  // });
+    app.enableCors();
 
   // Swagger
   const swaggerConfig = new DocumentBuilder()
@@ -38,5 +33,4 @@ async function bootstrap() {
 }
 bootstrap();
 
-bootstrap();
 
